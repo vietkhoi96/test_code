7b386de7146c79e232c36deb871e3702b3ddb46f
khoinv <vietkhoi777@gmail.com>
2023-01-05 17:36:31 +0700

    update

--- convert_image_to_json.py
+++ convert_image_to_json.py
@@ -1,11 +1,12 @@
 from PIL import Image
 from pytesseract import pytesseract
 import json
+a =[]
 path_to_image = '/home/khoinv/Downloads/excel.jpg'
 img = Image.open(path_to_image)
 text = pytesseract.image_to_string(img)
-for i in 
-with open('text.txt', 'w') as f:
-    f.write(text)
-file = '/home/khoinv/text.txt'
-dict = {}
+#with open('text.txt', 'w') as f:
+#    f.write(text)
+print(text)
+#a.append(text)
+#print(a)
